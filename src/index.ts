fde463756386987b3fe97c0c690179d58858a68f
Marc Blank <hello.marcblank@gmail.com>
2024-06-01 17:41:50 +0200

    fixed dependencies and added index file

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('This is your page.');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
+
+console.log("Hello friend!");
